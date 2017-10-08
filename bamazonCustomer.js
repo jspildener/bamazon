ecb57de01a35bdf0287d5933499538a6d2fd4d04
William R Poulson <jessica.spildener@gmail.com>
2017-10-08 10:42:12 -0400

    inquirer added

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,12 +17,35 @@ connection.connect(function(err) {
 
 function displayItems() {
     connection.query("SELECT * FROM products", function(err, results) {
-            if (err) throw err;
-            console.log("Current Inventory Available: " + "\n");
-            for (var i = 0; i < results.length; i++) {
-                console.log("Product Id: " + results[i].item_id + " Product Name: " +
-                    results[i].product_name + " Price: " + results[i].price);
+        if (err) throw err;
+        console.log("Current Inventory Available: " + "\n");
+        for (var i = 0; i < results.length; i++) {
+            console.log("Product Id: " + results[i].item_id + " Product Name: " +
+                results[i].product_name + " Price: " + results[i].price);
+        }
+        inquirer.prompt([{
+                type: "input",
+                message: "What is the ID of the item you would like to purchase?",
+                name: "id",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                type: "input",
+                message: "How many would you like to purchase?",
+                name: "amount",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
             }
+        ])
     });
 }
 displayItems();
